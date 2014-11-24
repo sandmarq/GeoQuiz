90c7b49aef6c1e7049113b3c10c9406e1118e713
sandmarq <sandrine.marquis30@gmail.com>
2014-11-24 15:44:26 -0500

    Chap 2 Mid

---
+++ app/src/main/java/ca/sandmarq/geoquiz/geoquiz/TrueFalse.java
@@ -0,0 +1,33 @@
+package ca.sandmarq.geoquiz.geoquiz;
+
+/**
+ * Created by SANDRINE.MARQUIS on 2014-11-24.
+*/
+public class TrueFalse {
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    private int mQuestion;
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+
+}
