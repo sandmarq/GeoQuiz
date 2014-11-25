fcc297a12dd5b68630a3b962355edb97a54a90f5
sandmarq <sandrine.marquis30@gmail.com>
2014-11-25 11:41:37 -0500

    Chap 5 mid

---
+++ app/src/main/java/ca/sandmarq/geoquiz/geoquiz/CheatActivity.java
@@ -0,0 +1,15 @@
+package ca.sandmarq.geoquiz.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SANDRINE.MARQUIS on 2014-11-25.
+ */
+public class CheatActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
--- app/src/main/java/ca/sandmarq/geoquiz/geoquiz/QuizActivity.java
+++ app/src/main/java/ca/sandmarq/geoquiz/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package ca.sandmarq.geoquiz.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,8 @@ public class QuizActivity extends ActionBarActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
+
     private TextView mQuestionTextView;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -85,6 +88,15 @@ public class QuizActivity extends ActionBarActivity {
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
+
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(i);
+            }
+        });
 
         updateQuestion();
     }
